f42d64412ce71c1a390fb25981fa835cb83c59e1
Elint Eerie <96844092+elinteerie@users.noreply.github.com>
2022-06-24 01:08:42 +0100

    added: load_and_prep_image
    added: load_and_prep_image

--- helper_function.py
+++ helper_function.py
@@ -359,3 +359,27 @@ def create_model_checkpoint(file_name):
                                                          save_freq ='epoch',
                                                          verbose =1)
   return checkpoint_callback
+
+
+def load_and_prep_image(filename, img_shape=224, scale=True):
+  """
+  Reads in an image from filename, turns it into a tensor and reshapes into
+  (224, 224, 3).
+
+  Parameters
+  ----------
+  filename (str): string filename of target image
+  img_shape (int): size to resize target image to, default 224
+  scale (bool): whether to scale pixel values to range(0, 1), default True
+  """
+  # Read in the image
+  img = tf.io.read_file(filename)
+  # Decode it into a tensor
+  img = tf.io.decode_image(img)
+  # Resize the image
+  img = tf.image.resize(img, [img_shape, img_shape])
+  if scale:
+    # Rescale the image (get all values between 0 and 1)
+    return img/255.
+  else:
+    return img
